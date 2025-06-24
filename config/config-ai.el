f2a4644b1bd40c9b65e31ee139c0f6161b65f6b4
Simon Van Casteren <simon.van.casteren@gmail.com>
2025-06-24 15:49:31 +0200

    disable aidermacs

--- config/config-ai.el
+++ config/config-ai.el
@@ -1,17 +1,17 @@
 ;; Use https://github.com/karthink/gptel !!
 
-(use-package aidermacs
-  :straight t
-  ;; :bind (("C-c a" . aidermacs-transient-menu))
-  :config
-  ;; (setenv "GEMINI_API_KEY" (getenv "GOOGLE_CLOUD_PLATFORM_API_KEY"))  ;; See .secrets
-  (setq aidermacs-program "/nix/store/j3jchsf49pzlik84cqjgg41sd5zrz16v-python3.12-aider-chat-0.82.1/bin/aider")
-  ;; (setq aidermacs-default-model "gemini/gemini-2.5-flash-preview-04-17")
-  ;; (setq aidermacs-default-model "gemini/gemini-2.5-pro-preview-03-25")
-  ;; :custom
-  ;; set in .env file
-  (setq aidermacs-use-architect-mode 1)
-  )
+;; (use-package aidermacs
+;;   :straight t
+;;   ;; :bind (("C-c a" . aidermacs-transient-menu))
+;;   :config
+;;   ;; (setenv "GEMINI_API_KEY" (getenv "GOOGLE_CLOUD_PLATFORM_API_KEY"))  ;; See .secrets
+;;   (setq aidermacs-program "/nix/store/j3jchsf49pzlik84cqjgg41sd5zrz16v-python3.12-aider-chat-0.82.1/bin/aider")
+;;   ;; (setq aidermacs-default-model "gemini/gemini-2.5-flash-preview-04-17")
+;;   ;; (setq aidermacs-default-model "gemini/gemini-2.5-pro-preview-03-25")
+;;   ;; :custom
+;;   ;; set in .env file
+;;   (setq aidermacs-use-architect-mode 1)
+;;   )
 
 
 (use-package gptel
@@ -21,7 +21,7 @@
   (setq
    gptel-model 'gemini-2.5-pro-preview-05-06
    gptel-backend (gptel-make-gemini "Gemini"
-                   :key (getenv "GOOGLE_CLOUD_PLATFORM_API_KEY")
-                   :stream t))
+                                    :key (getenv "GOOGLE_CLOUD_PLATFORM_API_KEY")
+                                    :stream t))
   (evil-define-key 'visual eglot-mode-map (kbd ", i") 'gptel-rewrite)
   )
