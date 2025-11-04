fe1c624dbec3e3f83d9d79062b3e84bb6978b585
Simon Van Casteren <simon.vancasteren@nobi.life>
2025-11-04 12:58:03 +0100

    darwin

--- config/config-vterm.el
+++ config/config-vterm.el
@@ -1,6 +1,11 @@
 ;; -*- lexical-binding: t -*-
 (setq vterm-max-scrollback 5000)
 
+(if (eq system-type 'darwin)
+    (use-package vterm
+      :straight t)
+  )
+
 (defun vterm-new ()
   (interactive)
   (projectile-run-vterm t)
